554292caf25a0689873bd1da7dd3ed5d9906909e
Tamreen Khan <historium@gmail.com>
2012-03-04 18:54:01 -0500

    Add recent api call.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-chartbeat "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.2"]])
--- src/clj_chartbeat/core.clj
+++ src/clj_chartbeat/core.clj
@@ -1,1 +1,14 @@
-(ns clj-chartbeat.core)
+(ns clj-chartbeat.core
+  (:require [clj-http.client :as client])
+  (:use [clojure.string :only [join]]))
+
+(def live-url "http://api.chartbeat.com/live/")
+(def p {"api-key" "3d56df86832d3188b01d4ae8613427cb"
+        "host" "scriptor.github.com"})
+
+(defn build-url [api-call]
+  (str live-url api-call "/?"))
+
+(defn recent []
+  (client/get (build-url "recent")
+              {:query-params p}))
